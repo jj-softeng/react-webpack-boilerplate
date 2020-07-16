b3538f0e6746262c219689693c22bb063d3fa244
jj-softeng <jj.softeng@gmail.com>
2020-07-16 07:19:52 +0500

    seperated global styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ export const App = () => (
   <React.Fragment>
     <div className="container">
       <div>
-        <h1>Hello React Developers!</h1>
+        <h1>Welcome Developers!</h1>
         <div className="logo">
           <img className="logo-img" src={Logo} alt="logo" />
         </div>
         <p className="bottom">
           Prepaired by&nbsp;{" "}
           <span>
-            <a href="https://github.com/jj-softeng/">Junaid</a>
+            <a href="https://github.com/jj-softeng/">Junaid Javed</a>
           </span>
         </p>
       </div>
